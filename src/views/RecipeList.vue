e588da322899100cd83dc662c6ce1fffe2dcc2bf
Sylvain Bodin <ky.schweitzer@gmail.com>
2020-06-18 21:32:06 +0200

    Ajout d'un message en cas de liste vide

--- src/views/RecipeList.vue
+++ src/views/RecipeList.vue
@@ -1,46 +1,55 @@
 <template>
     <div class="content">
-        <div class="has-text-centered">
-            <b-button
-                    tag="router-link"
-                    to="/recipe/create"
-                    size="is-large"
-                    icon-right="plus"
-                    data-test="add-recipe"
-            >
-                Ajouter une recette
-            </b-button>
-        </div>
         <div class="content">
             <b-skeleton
                     width="50%"
                     :active="loading"
             ></b-skeleton>
-            <div data-test="recipe-list" v-if="!loading">
-                <article
-                        class="media"
-                        v-for="recipe in recipes"
-                        :key="recipe._id"
-
-                >
-                    <figure class="media-left">
-                        <p class="image is-48x48">
-                            <img :src="recipe.image" alt="Aperçu de la recette">
-                        </p>
-                    </figure>
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-                                <router-link
-                                        :to="{name:'Recipe', params:{id: recipe._id}}"
-                                >
-                                    <strong>{{recipe.name}}</strong>
-                                </router-link>
-                            </p>
-                        </div>
-
+            <div v-if="!loading">
+                <div v-if="recipes.length === 0">
+                    <p class="is-size-3 has-text-centered">
+                        Oups !<br/>
+                        Il semblerait que vous n'ayez pas encore de recettes.<br/>
+                        Cliquez ici pour en ajouter.
+                    </p>
+                    <div class="has-text-centered">
+                        <b-button
+                                tag="router-link"
+                                to="/recipe/create"
+                                size="is-large"
+                                icon-right="plus"
+                                data-test="add-recipe"
+                        >
+                            Ajouter une recette
+                        </b-button>
                     </div>
-                </article>
+                </div>
+                <div data-test="recipe-list">
+                    <article
+                            class="media"
+                            v-for="recipe in recipes"
+                            :key="recipe._id"
+
+                    >
+                        <figure class="media-left">
+                            <p class="image is-48x48">
+                                <img :src="recipe.image" alt="Aperçu de la recette">
+                            </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <router-link
+                                            :to="{name:'Recipe', params:{id: recipe._id}}"
+                                    >
+                                        <strong>{{recipe.name}}</strong>
+                                    </router-link>
+                                </p>
+                            </div>
+
+                        </div>
+                    </article>
+                </div>
             </div>
         </div>
 
